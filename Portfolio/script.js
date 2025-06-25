3d6eb8264bfdd127058de931859aa9434f0972aa
Hpyloripos <Georgschwarz1@gmx.de>
2025-06-25 18:08:56 +0200

    ordner

---
+++ Portfolio/script.js
@@ -0,0 +1,14 @@
+//  alert('Hello World') 
+     
+  /*   const numberOne = 1;
+     const isEighteen = confirm('Are you over 18?')
+      console.log(isEighteen);
+     
+    // const name=prompt('What is your Name?');
+    // alert(name); */
+
+  //  let theNumber = 42;
+   // theNumber = theNumber + 1;
+    
+   // console.log(theNumber + 1);
+
