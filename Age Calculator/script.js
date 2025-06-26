39c824838ab95b719c9071dcf9e72a949d9d3e12
Hpyloripos <Georgschwarz1@gmx.de>
2025-06-26 14:57:07 +0200

    lebendetage

--- Age Calculator/script.js
+++ Age Calculator/script.js
@@ -1,3 +1,36 @@
+let userAge=''
+let userName=''
+let birthYear=''
+let daysAlive=''
+const afterJesus=2025
+const daysYear=365
+
 function calculateAge() {
- let  'What is yoour Name?';
+ userAge=prompt('what is your Age?');
+userAge= parseInt(userAge);
+
+}
+
+function getName() {
+ userName=prompt('What is your Name?');
+}
+
+function userGreet() {
+     getName()
+    calculateAge() 
+   daysLived()
+alert('Hello ' + userName + '!'+'You are ' + userAge +' yeras old.' + 
+      ' You were born in ' + birthYear + '.'+ 'You have been alive for approximately '+ daysAlive +'.');
+
+
+
+
+}
+
+function daysLived() {
+     birthYear= afterJesus - userAge;
+    daysAlive= userAge * daysYear;
+
+
+
 }
